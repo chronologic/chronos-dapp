1436e906535f8bdac642d3ca0e831b6c8fef4d16
shuchityagi <shuchit91@gmail.com>
2017-10-16 14:27:05 +0530

    Token watching API

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app     = express();
 var tokenABI = require('./tokenABI.json');
 var bytecode = require('./bytecode.json');
+var daytokenABI = require('./dayTokenABI.json');
+var daytokenbytecode = require('./dayTokenBytecode.json');
 
 app.set('view engine', 'pug');
 
@@ -9,6 +11,10 @@ app.set('view engine', 'pug');
 app.get('/', function (req, res) {
     res.render('index', {'tokenABI':tokenABI, 'bytecode_data':bytecode.data});
   })
+
+app.get('/watch/:address', function (req, res) {
+    res.render('watchToken', {'tokenAddress':req.params.address,'tokenABI':daytokenABI, 'bytecode_data':daytokenbytecode.data});
+  })  
 
 app.listen(3000);
 console.log("Running at Port 3000");
