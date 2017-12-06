aa6e9cfb06568c06fe714a7f97e6e7b0a006073a
Adibas03 <adibas03@ymail.com>
2017-12-06 12:18:34 +0100

    clean up next _document (html template)

--- pages/_document.js
+++ pages/_document.js
@@ -1,49 +1,6 @@
 import React from 'react';
 import Document, { Main, NextScript } from 'next/document';
 import stylesheet from 'styles/index.scss';
-import Prefix from 'utils/prefixHelper';
-import htmlescape from 'htmlescape';
-
-class PrefixedNextScript extends NextScript {
-  render() {
-    const { __NEXT_DATA__ } = this.context._documentProps;
-    const { prefix } = this.props;
-
-    const env = process.env.NODE_ENV;
-
-    // script element contain development js
-    let scriptElements = (
-      <div>
-        <script type="text/javascript" src={`${prefix}/_next/-/manifest.js`} />
-        <script type="text/javascript" src={`${prefix}/_next/-/commons.js`} />
-        <script type="text/javascript" src={`${prefix}/_next/-/main.js`} />
-      </div>
-    );
-
-    if (env === 'production') {
-      const { buildStats } = __NEXT_DATA__;
-      const app = 'app.js';
-
-      // script element contain production js
-      scriptElements =
-        <script
-          type="text/javascript"
-          src={`${prefix}/_next/${buildStats[app].hash}/${app}`}
-        />
-    }
-
-    return (
-      <div>
-        <script
-          dangerouslySetInnerHTML={
-            { __html: `__NEXT_DATA__ = ${htmlescape(__NEXT_DATA__)}; module={};` }
-          }
-        />
-        {scriptElements}
-      </div>
-    );
-  }
-}
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
@@ -55,15 +12,15 @@ export default class MyDocument extends Document {
     };
   }
 
+
   render() {
-    console.log('config',Prefix)
     return (
       <html lang="en">
         <body>
           <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
           {this.props.customValue}
           <Main />
-          <PrefixedNextScript prefix={Prefix} />
+          <NextScript />
         </body>
       </html>
     );
