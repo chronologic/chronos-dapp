b8672f5ef96d53672cf05c66ddb45e93d0ef14a8
Adibas03 <adibas03@ymail.com>
2017-12-07 19:20:48 +0100

    Update favicon display logic

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <html lang="en">
+        {<link rel="shortcut icon" href={process.env.BACKEND_URL + "/favicon.ico"} type="image/x-icon" />}
         <body>
           <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
           {this.props.customValue}
