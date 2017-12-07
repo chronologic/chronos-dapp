79f978abf1b961be6c3edad99e03ec1697ac7905
Adibas03 <adibas03@ymail.com>
2017-12-07 16:50:01 +0100

    update new repo directory

--- env-config.js
+++ env-config.js
@@ -2,5 +2,5 @@ const stage = process.env.NODE_ENV === 'staging';
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.BACKEND_URL':stage?'/custom-day-dapp':''
+  'process.env.BACKEND_URL':stage?'/chronos-dapp':''
 }
