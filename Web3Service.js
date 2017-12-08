06d0549d4c522b9b3c15c8ae5a9205821bf8e98c
Adibas03 <adibas03@ymail.com>
2017-12-08 13:44:58 +0100

    begin implementing web3 deployment process

--- Web3Service.js
+++ Web3Service.js
@@ -3,6 +3,7 @@ import Bb from 'bluebird';
 import { action, observable, runInAction } from 'mobx';
 
 import dayTokenABI from './abi/dayTokenABI';
+import deployerABI from './abi/deployerABI';
 
 const TOKEN_CONTRACT_ADDRESS = '0x7941bc77E1d6BD4628467b6cD3650F20F745dB06';
 const DEPLOYER_ADDRESS = '0x0B482E31ff16143719414Afa1EF102C6B39178F4';
@@ -14,6 +15,7 @@ export default class Web3Service {
   initialized = false;
   web3 = null;
   tokenInstance = null;
+  deployerInstance = null;
   @observable connectedToMetaMask = null;
   @observable accounts = null;
   @observable netId = null;
@@ -60,6 +62,7 @@ export default class Web3Service {
     });
     console.log('netId', this.netId);
     this.tokenInstance = web3.eth.contract(dayTokenABI).at(TOKEN_CONTRACT_ADDRESS);
+    this.deployerInstance = web3.eth.contract(deployerABI).at(DEPLOYER_ADDRESS);
   }
 
   @action
@@ -76,6 +79,67 @@ export default class Web3Service {
       this.tokenInstance.approve(DEPLOYER_ADDRESS, MIN_FEE, callback);
     });
     return result;
+  }
+
+  @action
+  async deploy() {
+    const result =  await this.sendTransaction();
+    console.log(result);
+  }
+
+  @action
+  async sendTransaction() {
+      console.log(this)
+    /*
+    console.log("deploying new token");
+    var _tokenName = $("#name").val();
+    var _tokenSymbol = $("#symbol").val();
+    var _maxAddresses = $("#maxAddresses").val();
+    var _firstTeamContributorId = $("#firstTeamContributionId").val();
+    var _totalTeamContributorIds = $("#totalTeamContributionId").val();
+    var _totalPostIcoContributorIds = $("#totalPostIcoContributorIds").val();
+    var _minMintingPower = $("#minMintingPower").val();
+    var _maxMintingPower = $("#maxMintingPower").val();
+    var _halvingCycle = $("#halvingCycle").val();
+    var _DayInSecs = $('#dayLengthinSec').val();
+    var _minBalanceToSell = $("#teamLockPeriodInSec").val();
+    var _teamLockPeriodInSec = $("#minBalanceToSell").val();
+getDeployerInstance().then(function(deployerInstance) {
+    var txHash = deployerInstance.createCustomDayToken(_tokenName,_tokenSymbol,_maxAddresses,_firstTeamContributorId,_totalTeamContributorIds,_totalPostIcoContributorIds,_minMintingPower,_maxMintingPower,_halvingCycle,_minBalanceToSell,_DayInSecs, _teamLockPeriodInSec,{
+            from: accounts[0],
+            gas: 4100000,
+            value: 0
+        },function(error,result){
+            if(result){
+                txUrl = baseNetworkURL+"tx/"+result;
+
+                swal({
+                title: 'Please wait while the transaction is mined',
+                html: '<a target="_blank" href="'+txUrl+'">'+result+'</a>',
+                allowOutsideClick:false,
+                type: "info",
+                showCancelButton: false,
+                showConfirmButton: false,
+                allowEscapeKey:false
+                })
+                return awaitBlockConsensus(window.web3, result,2, 200).then(function(result){
+                    var obj = result.logs;
+                    window.location.replace("watchToken.html?tad="+obj[2].address);
+
+            }, function(error) {
+                console.log('Could not fetch new balances', error);
+                return Promise.reject(error);
+            });
+            }
+            else
+                return;
+        });
+    }).then(function(result){
+        console.log("result",result);
+    }, function(error) {
+        console.log('Could not fetch new balances', error);
+        return Promise.reject(error);
+    });*/
   }
 }
 
