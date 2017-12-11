339c9817b4ad412eb0072dfeed4f7548fd60a4a2
Adibas03 <adibas03@ymail.com>
2017-12-11 20:22:44 +0100

    clean up

--- components/InputField.js
+++ components/InputField.js
@@ -24,7 +24,6 @@ export default class InputField extends React.Component {
       valid,
       value,
     } = this.props;
-    console.log(description)
 
     const error = !valid ? errorMessage : '';
     return (
