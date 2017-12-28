e038db11f80c580cc21c2835688390dc6854fc94
Adibas03 <adibas03@ymail.com>
2017-12-28 23:17:06 +0100

    move actual deployment to step-4

--- components/Step4.js
+++ components/Step4.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { observable } from 'mobx';
 import { inject, observer } from 'mobx-react';
+import ReactTooltip from 'react-tooltip'
 
 import { PROPERTIES as ALL_PROPERTIES } from '../lib/consts';
+import {showError} from '../lib/alerts';
+import web3Config from '../lib/web3Utils';
 import AbstractStep from './AbstractStep';
 import StepLayout from './StepLayout';
+
+import Ringloader,{Boxloader} from '../lib/loader';
 
 @inject('web3Service')
 @inject('store')
@@ -12,6 +17,7 @@ import StepLayout from './StepLayout';
 export default class Step4 extends AbstractStep {
   constructor(props) {
     super('PUBLISH', props);
+    this.runDeploy = this.runDeploy.bind(this);
   }
 
   getValidations() {
@@ -34,6 +40,28 @@ export default class Step4 extends AbstractStep {
     minimumBalance: true,
   };
 
+  @observable
+  _state = {
+    notReady: true,
+    deploying: false,
+    contractInstance:{},
+    deploymentData:{}
+  }
+
+  goNext = () => {
+    const { props: { store } } = this;
+    let that = this;
+    const CONTRACT_PROPERTIES = ['transactionHash','newContract']
+    const query = CONTRACT_PROPERTIES.reduce((result, { name }) => {
+      result[name] = that._state[name];
+      return result;
+    }, {});
+    Router.push({
+      pathname: this.activeStep.nextUrl,
+      query,
+    });
+  };
+
   renderProperty(propertyData, otherProps = {}) {
     const { props: { store } } = this;
     const {
@@ -47,40 +75,135 @@ export default class Step4 extends AbstractStep {
     );
   }
 
+  async runDeploy(eventInst){
+    var target = eventInst.target
+    const {web3Service} = this.props;
+    const {web3} = web3Service;
+    if(this.web3Disabled(web3Service) )
+      return;
+    target.disabled = true;
+    this.setState( Object.assign(this._state,{deploying:true}) );
+
+    try{
+      //const transaction = '0x9c7ee592dee97d515458ff4ca6e441db6f2a016bb821c40bde4136f7a264b22a';
+      const transaction = await web3Service.deploy( this.fetchData() );
+      const newContract = await this.contractDeployed(transaction);
+      this.setState( Object.assign(this._state,{deploying:false}) );
+    }
+    catch(e){
+      target.disabled = false;
+      console.error(e);
+      showError('There was problem deploying the contract.');
+    }
+
+    this.setState( Object.assign(this._state,{deploying:false,notReady:false}) );
+
+    if(typeof newContract != 'undefined' && web3.isAddress(newContract) ){
+      this.setState( Object.assign(this._state,{newContract:newContract}) );
+      this.goNext();
+    }
+    else{
+      const error = await showError('There was problem deploying the contract. Try again?');
+
+      if(error)
+      target.disabled = false;
+    }
+  }
+
+  async contractDeployed( transaction ){
+    const {web3Service,store} = this.props;
+    this.setState( Object.assign(this._state,{transactionHash:transaction,notReady:true}) );
+    const mined = await this.awaitMined(transaction);
+    this.setState( Object.assign(this._state,{transactionReceipt:mined}) );
+    const confirmations = await this.checkConfirmations(transaction);
+    this.setState( Object.assign(this._state,{notReady:false}) );
+    const contract = await web3Service.fetchNewChild(transaction);
+    return contract;
+  }
+
+  async awaitMined (transaction){
+     const {web3Service} = this.props;
+     const mined = await web3Service.trackTransaction(transaction);
+     return mined;
+  }
+
+  async checkConfirmations (transaction){
+    const {web3Service} = this.props;
+    const confirmations = await web3Service.fetchConfirmations(transaction);
+    console.log(confirmations)
+    if(confirmations < 1 )
+      return await this.checkConfirmations(transaction);
+    else{
+      this.setState( Object.assign(this._state,{loadingData:true}) );
+      return confirmations;
+    }
+  }
+
+  fetchData(){
+    const { props: { store }, activeStep: { propertyKeys } } = this;
+    var data = {};
+    propertyKeys.map(function(field){
+      data[field] = store[field]
+    })
+    return data;
+  }
+
   render() {
 
     const {web3Service} = this.props;
+    const EXPLORER = web3Config[web3Service.network].EXPLORER;
+
     return (
       <StepLayout
         activeStepKey={this.activeStepKey}
-        onNext={this.goNext}
+        onNext={this.runDeploy}
         nextTitle="Deploy"
         web3Disabled = {this.web3Disabled(web3Service) }
       >
-        <div className="input-block-container">
-          {this.renderProperty(this.properties.tokenName, { side: 'left' })}
-          {this.renderProperty(this.properties.symbol, { side: 'right' })}
-        </div>
-        <div className="input-block-container">
-          {this.renderProperty(this.properties.minMintingPower, { side: 'left' })}
-          {this.renderProperty(this.properties.maxMintingPower, { side: 'right' })}
-        </div>
-        <div className="input-block-container">
-          {this.renderProperty(this.properties.halvingCycle, { side: 'left' })}
-          {this.renderProperty(this.properties.mintingPeriod, { side: 'right' })}
-        </div>
-        <div className="input-block-container">
-          {this.renderProperty(this.properties.maxAddresses, { side: 'left' })}
-          {this.renderProperty(this.properties.startingId, { side: 'right' })}
-        </div>
-        <div className="input-block-container">
-          {this.renderProperty(this.properties.totalMintingId, { side: 'left' })}
-          {this.renderProperty(this.properties.teamLockPeriod, { side: 'right' })}
-        </div>
-        <div className="input-block-container">
-          {this.renderProperty(this.properties.postDeploymentMaxIds, { side: 'left' })}
-          {this.renderProperty(this.properties.minimumBalance, { side: 'right' })}
-        </div>
+        {!this._state.deploying &&
+          <div>
+            <div className="input-block-container">
+              {this.renderProperty(this.properties.tokenName, { side: 'left' })}
+              {this.renderProperty(this.properties.symbol, { side: 'right' })}
+            </div>
+            <div className="input-block-container">
+              {this.renderProperty(this.properties.minMintingPower, { side: 'left' })}
+              {this.renderProperty(this.properties.maxMintingPower, { side: 'right' })}
+            </div>
+            <div className="input-block-container">
+              {this.renderProperty(this.properties.halvingCycle, { side: 'left' })}
+              {this.renderProperty(this.properties.mintingPeriod, { side: 'right' })}
+            </div>
+            <div className="input-block-container">
+              {this.renderProperty(this.properties.maxAddresses, { side: 'left' })}
+              {this.renderProperty(this.properties.startingId, { side: 'right' })}
+            </div>
+            <div className="input-block-container">
+              {this.renderProperty(this.properties.totalMintingId, { side: 'left' })}
+              {this.renderProperty(this.properties.teamLockPeriod, { side: 'right' })}
+            </div>
+            <div className="input-block-container">
+              {this.renderProperty(this.properties.postDeploymentMaxIds, { side: 'left' })}
+              {this.renderProperty(this.properties.minimumBalance, { side: 'right' })}
+            </div>
+          </div>
+        }
+        {this._state.deploying &&
+         <div className="input-block-container">
+          <Boxloader {...{color:'#123abc',loading: true, size:13,msg:!this._state.transactionHash?'Deploying...':'Awaiting Mining ...'}} />
+         </div>
+        }
+        {this._state.deploying && !this._state.notReady &&
+          <div className="input-block-container center text-center">
+            <p className='loading_msg' >Successfully deployed ...</p>
+          </div>
+        }
+        {this._state.deploying && this._state.transactionHash &&
+          <div className="input-block-container value center text-center">
+            <label className="label">Transaction Hash : </label>
+            <a target="_blank" href={EXPLORER+'/tx/'+this._state.transactionHash}>{this._state.transactionHash}</a>
+          </div>
+        }
       </StepLayout>
     );
   }
