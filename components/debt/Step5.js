8e72bb7b61ae3e8bb7a98afbc116d89b4ac82053
coderwithattitude <coderwithattitude@gmail.com>
2018-01-16 19:44:48 +0100

    added Contract data populater function

--- components/debt/Step5.js
+++ components/debt/Step5.js
@@ -9,8 +9,11 @@ import {showError} from '../../lib/alerts';
 import web3Config from '../../lib/web3Utils';
 import AbstractStep from '../AbstractStep';
 import StepLayout from '../StepLayout';
-
+import { DEBT_CONTRACT_LABELS } from "../../lib/consts";
+import {showError,showInfo,confirmProcess} from "../../lib/alerts";
 import {Boxloader} from '../../lib/loader';
+
+import {Propagatesloader} from "../../lib/loader";
 
 @inject('web3Service')
 @inject('store')
@@ -21,7 +24,26 @@ export default class Step5 extends AbstractStep {
         this.runDeploy = this.runDeploy.bind(this);
     }
 
-
+    const ContractData = data => {
+        const explorer = data.explorer;
+        data = data.data;
+        let Data = [],
+            index = 0;
+        for(let d in data){
+            if(d=='address')
+                Data.push(<div className={'col col-3'} key={d}>
+                    <label className="label">{DEBT_CONTRACT_LABELS[d]+' : '}</label>
+                    <p className='' ><a target="_blank" href={explorer+'address/'+data[d]}>{ data[d] }</a></p>
+                </div>);
+            else
+                Data.push(<div className={'col col-3'} key={d}>
+                    <label className="label">{DEBT_CONTRACT_LABELS[d]+' : '}</label>
+                    <p className='' >{ data[d] }</p>
+                </div>);
+            index++;
+        }
+        return (Data);
+    }
 
 
 
@@ -32,6 +54,6 @@ export default class Step5 extends AbstractStep {
 
 
     render() {
-        
+
     }
 }
