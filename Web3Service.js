b45a0b300f4429bad5c7003567084d3b3e96775a
Adibas03 <adibas03@ymail.com>
2018-01-25 17:11:28 +0100

    add TotalSupply

--- Web3Service.js
+++ Web3Service.js
@@ -607,6 +607,7 @@ export default class Web3Service {
                     address: contract,
                     tokenName: (await Bb.fromCallback(callback => debtContract.name.call(callback))).valueOf(),
                     symbol: (await Bb.fromCallback(callback => debtContract.symbol.call(callback))).valueOf(),
+                    totalSupply: this.finePrint((await Bb.fromCallback(callback => debtContract.totalSupply.call(callback))).valueOf()),
                     dayLength: this.finePrint((await Bb.fromCallback(callback => debtContract.dayLength.call(callback))).valueOf()),
                     loanTerm: this.finePrint((await Bb.fromCallback(callback => debtContract.loanTerm.call(callback))).valueOf()),
                     exchangeRate: this.finePrint((await Bb.fromCallback(callback => debtContract.exchangeRate.call(callback))).valueOf()),
